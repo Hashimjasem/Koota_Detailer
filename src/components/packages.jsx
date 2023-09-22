ab64f0c0e8d102ef6c9dd7dbc1fa67203fbba4ca
Hashimjasem <j_hashim1305@outlook.com>
2023-09-22 13:59:56 +0800

    can now add text infront of image in packages

--- src/components/packages.jsx
+++ src/components/packages.jsx
@@ -2,19 +2,52 @@ function Packages() {
     return (
         <div className='bg-[#181520]'>
             <p className="text-white text-[2rem] text-center">Packages</p>
-                  <div className="body">
-                    <div className="boxext">
-                        
-                    </div>
-  
-                    <div className="boxint">
 
-                    </div>
 
-                    <div className="boxprem">
-
-                    </div>
-                    </div>      
+            <div class="exterior-box">
+                <div className="info-box">
+                    <h1 className="package-title exterior"> Exterior Detail</h1>
+                    <ul>
+                        <li>Foam Canon</li>
+                        <li>Full Polish</li>
+                        <li>Spotless Mirror Finish</li>
+                        <li>Wheels + Tire + Breaks Cleaned</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="interior-box">
+                <div className="info-box">
+                    <h1 className="package-title"> Intterior Detail</h1>
+                    <ul>
+                        <li>No Compromise Interior Detail</li>
+                        <li>Full Polish</li>
+                        <li>Spotless Mirror Finish</li>
+                        <li>Wheels + Tire + Breaks Cleaned</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="premium-box">
+                <div className="info-box">
+                    <h1 className="package-title"> Exterior Detail</h1>
+                    <ul>
+                        <li>Foam Canon</li>
+                        <li>Full Polish</li>
+                        <li>Spotless Mirror Finish</li>
+                        <li>Wheels + Tire + Breaks Cleaned</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="extras-box">
+                <div className="info-box">
+                    <h1 className="package-title"> Exterior Detail</h1>
+                    <ul>
+                        <li>Foam Canon</li>
+                        <li>Full Polish</li>
+                        <li>Spotless Mirror Finish</li>
+                        <li>Wheels + Tire + Breaks Cleaned</li>
+                    </ul>
+                </div>
+            </div>
         </div>
     );
 }
